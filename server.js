a119b9b5d359672378b091411a8d52db63656659
Rony Rizk <rony_rizk@live.com>
2022-12-10 12:28:56 +0200

    Added a new endpoint /data

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const PORT = process.env.PORT || 3030;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.get('/data', function (req, res) {
+    var data = [
+        {id:17,name:'Omar'},
+        {id:54,name:'Elia'},
+        {id:32,name:'Fatat'}
+    ]
+    res.send(data)
+  })
+
 app.listen(PORT)
